64d63ba340ab8fb46b7dfdd6270d13647211911f
Julien Capellari <julien.capellari@gmail.com>
2023-06-01 19:03:45 +0200

    feat: game of life

--- www/index.js
+++ www/index.js
@@ -1,3 +1,17 @@
-import * as wasm from "hello-wasm-pack";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+let last = 0;
+
+function loop(time) {
+    if (time - last > 200) {
+        pre.textContent = universe.render();
+        universe.tick();
+        last = time;
+    }
+
+    requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
