08b00fac3ab29e0ebdce9502db1197318c5d40fa
Julien Capellari <julien.capellari@neoxia.com>
2023-06-02 15:40:38 +0200

    feat: draw life in canvas

--- src/lib.rs
+++ src/lib.rs
@@ -50,12 +50,10 @@ impl Universe {
     }
 }
 
+// Public methods
 #[wasm_bindgen]
 impl Universe {
-    pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
+    pub fn new(width: u32, height: u32) -> Universe {
         let cells = (0..width * height)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
@@ -99,6 +97,18 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 }
 
--- www/index.js
+++ www/index.js
@@ -1,14 +1,49 @@
-import { Universe } from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const pre = document.getElementById("game-of-life-canvas");
-const universe = Universe.new();
+// Constants
+const CELL_SIZE = 5;
+const DEAD_COLOR = "#FFFFFFDD";
+const ALIVE_COLOR = "#000000";
+const FRAME_RATE = 50;
+
+// Setup universe
+const universe = Universe.new(256, 128);
+const height = universe.height();
+const width = universe.width();
+
+// Setup canvas
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.width = CELL_SIZE * width;
+canvas.height = CELL_SIZE * height;
+
+const ctx = canvas.getContext('2d');
+
+// Render loop
 let last = 0;
 
 function loop(time) {
-    if (time - last > 200) {
-        pre.textContent = universe.render();
-        universe.tick();
+    if (time - last > FRAME_RATE) {
         last = time;
+
+        // Update state
+        universe.tick();
+
+        // Draw cells
+        const cells = new Uint8Array(memory.buffer, universe.cells(), width * height);
+
+        ctx.beginPath();
+
+        for (let row = 0; row < height; ++row) {
+            for (let col = 0; col < width; ++col) {
+                const idx = row * width + col;
+
+                ctx.fillStyle = cells[idx] === Cell.Alive ? ALIVE_COLOR : DEAD_COLOR;
+                ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+        }
+
+        ctx.stroke();
     }
 
     requestAnimationFrame(loop);
